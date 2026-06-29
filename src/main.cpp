b2897e8f44e241a16ac00a4000e9fd61ad649081
Daynlight <danielstodulski000@gmail.com>
2026-06-29 19:35:18 +0100

    Runs on Debian :>

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
+#endif
 
 #include "application.h"
 #include "config.h"
 
 
+#if defined(_WIN32) || defined(_WIN64)
 extern "C" {
     __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 }
+#endif
 
 
 int main()
